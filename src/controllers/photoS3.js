1e720ec69b440dde72ac6660c5863dea6087ca8c
JessieChiu1 <jessiechiu12@gmail.com>
2024-01-31 22:17:37 -0500

    fixed reserved keyword

--- src/controllers/photoS3.js
+++ src/controllers/photoS3.js
@@ -5,8 +5,8 @@ const path = require("path")
 
 const bucketName = process.env.AWS_BUCKET_NAME
 const region = process.env.AWS_BUCKET_REGION
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+const accessKeyId = process.env.AWS_MY_ACCESS_KEY_ID
+const secretAccessKey = process.env.AWS_MY_SECRET_ACCESS_KEY
 
 const s3 = new S3({
     region,
