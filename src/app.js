5b197566f80c97fdadc392c79ab7b064ca59e878
JessieChiu1 <jessiechiu12@gmail.com>
2024-02-01 01:15:50 -0500

    revert changes

--- src/app.js
+++ src/app.js
@@ -1,22 +1,6 @@
 const express = require("express")
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-dotenv.config()
 
-const app = express()
-
-const PORT = process.env.PORT || 3001
-
-mongoose.set('strictQuery', false)
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.TEST_DB_URI)
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
-  } catch (error) {
-    console.log(error)
-    process.exit(1)
-  }
-}
+const app = express();
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
@@ -35,9 +19,3 @@ app.use("/api/v1/post", require("./routes/post"))
 app.use("/api/v1/photo", require("./routes/photo"))
 
 module.exports = app
-
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log("listening for requests")
-    })
-})
