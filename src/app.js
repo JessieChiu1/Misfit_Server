bd837404427b13229ab0db7e253b762b5ed6c181
JessieChiu1 <jessiechiu12@gmail.com>
2024-02-01 01:14:10 -0500

    tried to fix deployment issue

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+dotenv.config()
 
-const app = express();
+const app = express()
+
+const PORT = process.env.PORT || 3001
+
+mongoose.set('strictQuery', false)
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.TEST_DB_URI)
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
@@ -19,3 +35,9 @@ app.use("/api/v1/post", require("./routes/post"))
 app.use("/api/v1/photo", require("./routes/photo"))
 
 module.exports = app
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests")
+    })
+})
